85bfaff1de2a3c8b820bca94bf626f51df9fba58
Huda Yousif <hjdoski@gmail.com>
2019-10-09 20:08:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,7 @@
 def batch_badge_creator
   
   speakers =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  counter = 1
   speaker.each do |speaker|
     puts "Hello, #{speaker}! You'll be assigned to room _____!"
   
