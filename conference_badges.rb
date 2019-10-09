7ad8ef0bc09af96e8351531d093ad7723db47bc1
Huda Yousif <hjdoski@gmail.com>
2019-10-09 20:16:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,11 +6,11 @@ end
 
 
 
-def batch_badge_creator
-  
-  name =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(name)
+  new_array = []
   counter = 1
   speaker.each do |speaker|
     puts "Hello, #{speaker}! You'll be assigned to room #{counter}!"
   end
-end
+ puts new_array
+ end 
