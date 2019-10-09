15e59f0f8c0999082f2c583c9dd0425e7ec7aaea
Huda Yousif <hjdoski@gmail.com>
2019-10-09 20:12:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,9 @@
 def badge_maker(name) 
   name =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 badge_maker.each do |name|
-  
+  puts "Hello, my name is #{name}."
+end 
+
 
 def batch_badge_creator
   
